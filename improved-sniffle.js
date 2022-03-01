35ed301e8dae436bc2a29fe4fe84d3d1b911b679
J.Z. Bradley <jzbradley@gmail.com>
2022-02-28 23:32:11 -0800

    +fn curry

--- improved-sniffle.js
+++ improved-sniffle.js
@@ -1,24 +1,30 @@
+function curry(fn) {
+    return function curried(...args) {
+        return args.length < fn.length 
+        ? function(...args2) { return curried.apply(this, args.concat(args2)); }
+        : fn.apply(this, args);
+    };
+}
+function union(objects) {
+    return objects.reduce((l,r)=>Object.assign(l,r));
+}
+function outerApply(method,left,right) {
+  return [Object
+    .keys(right)
+    .filter(key=>typeof left[key]==='undefined')
+    .map(key=>({[key]:method(left[key])})),
+    Object
+    .keys(left)
+    .filter(key=>typeof right[key]==='undefined')
+    .map(key=>({[key]:method(right[key])}))
+  ].flat()
+    .reduce((l,r)=>Object.assign(l,r));
+}
 
-    function union(objects) {
-        return objects.reduce((l,r)=>Object.assign(l,r));
-    }
-    function outerApply(method,left,right) {
-      return [Object
-        .keys(right)
-        .filter(key=>typeof left[key]==='undefined')
-        .map(key=>({[key]:method(left[key])})),
-        Object
-        .keys(left)
-        .filter(key=>typeof right[key]==='undefined')
-        .map(key=>({[key]:method(right[key])}))
-      ].flat()
-        .reduce((l,r)=>Object.assign(l,r));
-    }
-
-  function innerApply(method,left,right) {
-    return Object
-      .keys(left)
-      .filter(key=>typeof right[key] !== 'undefined')
-      .map(key=>({[key]:method(left[key],right[key])}))
-      .reduce((l,r)=>Object.assign(l,r));
-  }
+function innerApply(method,left,right) {
+return Object
+  .keys(left)
+  .filter(key=>typeof right[key] !== 'undefined')
+  .map(key=>({[key]:method(left[key],right[key])}))
+  .reduce((l,r)=>Object.assign(l,r));
+}
